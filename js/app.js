0e63679d56bd3c6dd7d1b3b53af7cfd95a947287
Jeremy Deischer <41126368+jeremy-deischer@users.noreply.github.com>
2018-12-02 21:50:10 -0700

    Start adding basemap elements to js

--- js/app.js
+++ js/app.js
@@ -9,7 +9,20 @@
     console.log(error)
   } //end of function error
 
+  //Call all data files then send data to drawMap
+  Promise.all([basinJson]).then(drawMap)
+
   function drawMap(data){
     console.log(data) //access to data
+
+    // define width and height of our SVG
+      var width = 960,
+        height = 600
+
+      // select the map element
+      var svg = d3.select("#map")
+        .append("svg") // append a new SVG element
+        .attr("width", width) // give the SVS element a width attribute and value
+        .attr("height", height) // same for the height
   }
 });
