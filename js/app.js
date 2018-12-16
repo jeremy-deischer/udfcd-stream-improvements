e5b876b29dc355e99394ee6852ac7e1088231d26
Jeremy Deischer <41126368+jeremy-deischer@users.noreply.github.com>
2018-12-16 12:11:16 -0700

    Revise legend

--- js/app.js
+++ js/app.js
@@ -158,50 +158,53 @@
       })
       .addTo(map);
 
-    function drawLegend() {
-      //create new leaflet object and assign position
-      var legend = L.control({
-        position: 'bottomleft'
-      });
-
-      // when the legend is added to the map
-      legend.onAdd = function() {
-
-        // create a new HTML <div> element and give it a class name of "legend"
-        var div = L.DomUtil.create('div', 'legend');
-
-        // first append an <h3> tag to the div holding the current attribute
-        // and norm values (i.e., the mapped phenomena)
-        div.innerHTML = "<h3> Average County Rent Price</h3>";
-
-        // for each of our breaks
-        for (var i = 0; i < breaks.length; i++) {
-          // determine the color associated with each break value,
-          // including the lower range value
-          var color = getColor(breaks[i][0], breaks);
-
-          // concatenate a <span> tag styled with the color and the range values
-          // of that class and include a label with the low and a high ends of that class range
-          div.innerHTML +=
-            '<span style="background:' + color + '"></span> ' +
-            '<label>$' + breaks[i][0].toLocaleString() + ' &mdash; $' +
-            breaks[i][1].toLocaleString() + '</label>';
-        }
-
-        // return the populated div to be added to the map
-        return div;
-      };
-
-      // add the legend to the map
-      legend.addTo(map);
-
-    } //end of drawlengend
+    drawLegend();
 
     // add the filter using the streamsData
     addFilter(streamsData, streams, channelImproveData);
 
 
   } // end drawMap()
+
+  function drawLegend() {
+    //create new leaflet object and assign position
+    var legend = L.control({
+      position: 'bottomleft'
+    });
+
+    // when the legend is added to the map
+    legend.onAdd = function() {
+
+      // create a new HTML <div> element and give it a class name of "legend"
+      var div = L.DomUtil.create('div', 'legend');
+
+      // first append an <h3> tag to the div holding the current attribute
+      // and norm values (i.e., the mapped phenomena)
+      div.innerHTML = "<h3> Average County Rent Price</h3>";
+
+      // for each of our breaks
+      for (var i = 0; i < breaks.length; i++) {
+        // determine the color associated with each break value,
+        // including the lower range value
+        var color = getColor(breaks[i][0], breaks);
+
+        // concatenate a <span> tag styled with the color and the range values
+        // of that class and include a label with the low and a high ends of that class range
+        div.innerHTML +=
+          '<span style="background:' + color + '"></span> ' +
+          '<label>$' + breaks[i][0].toLocaleString() + ' &mdash; $' +
+          breaks[i][1].toLocaleString() + '</label>';
+      }
+
+      // return the populated div to be added to the map
+      return div;
+    };
+
+    // add the legend to the map
+    legend.addTo(map);
+
+  } //end of drawlengend
+
 
   // d3 to create dropdown of all the streams
   function addFilter(data, streams, channelImproveData) {
